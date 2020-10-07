cb8aff12b72f9586bc36d5e006cc4ae077ff017e
Francesco Ilario <fr.ilario@gmail.com>
2020-10-07 22:53:20 +0200

    Added firstUp/Low funcs

--- cg/fmap_string.go
+++ cg/fmap_string.go
@@ -12,4 +12,10 @@ var fmap = template.FuncMap{
 	"upper": func(text string) string {
 		return strings.ToUpper(text)
 	},
+	"firstUp": func(text string) string {
+		return strings.ToUpper(string(text[0])) + text[1:]
+	},
+	"firstLow": func(text string) string {
+		return strings.ToLower(string(text[0])) + text[1:]
+	},
 }
